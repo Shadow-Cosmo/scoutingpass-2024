2790cb7c1a18f90bcd62d755a80dd9e990e35a14
E_Chill <35980944+Eaz3@users.noreply.github.com>
2023-03-21 21:57:50 -0400

    updated event code

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -15,7 +15,7 @@ var config_data = `
 			"name": "Event",
 			"code": "e",
 			"type": "event",
-			"defaultValue": "2022carv",
+			"defaultValue": "2023mitry",
 			"required": "true",
 			"disabled": "true"
 		},
