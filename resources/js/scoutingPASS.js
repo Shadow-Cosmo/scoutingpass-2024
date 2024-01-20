0f55dd24f6f17e69d4c187265ba980df06578eff
E_Chill <35980944+Eaz3@users.noreply.github.com>
2024-01-19 19:10:45 -0500

    Update scoutingPASS.js
    690

--- resources/js/scoutingPASS.js
+++ resources/js/scoutingPASS.js
@@ -689,7 +689,7 @@ function addElement(table, idx, data) {
 
 function configure() {
   try {
-    var mydata = JSON.parse(config_data);
+    var mydata = JSON.parse(CU_config);
   } catch (err) {
     console.log(`Error parsing configuration file`)
     console.log(err.message)
